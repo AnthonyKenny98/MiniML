3cea3e0c85cd16ea33a329b92eebd9a545935c7e
Anthony Kenny <Anthony@Anthonys-Macbook-Pro-2106.local>
2017-04-28 21:03:23 -0400

    commiting some stuffff

--- expr.ml
+++ expr.ml
@@ -6,22 +6,24 @@
 
 (* Abstract syntax of MiniML expressions *)
 
+open Printf
+
 type unop =
-  | Negate
+  | Negate    
 ;;
     
 type binop =
-  | Plus
-  | Minus
-  | Times
-  | Equals
-  | LessThan
+  | Plus        (* "+" *)
+  | Minus       (* "-" *)
+  | Times       (* "*" *)   
+  | Equals      (* "=" *)
+  | LessThan    (* "<" *)
 ;;
       
 type expr =
-  | Var of varid                         (* variables *)
-  | Num of int                           (* integers *)
-  | Bool of bool                         (* booleans *)
+  | Var of varid                         (* variables - already a string *)
+  | Num of int                           (* integers - inttostring *)
+  | Bool of bool                         (* booleans - "true" "false" *)
   | Unop of unop * expr                  (* unary operators *)
   | Binop of binop * expr * expr         (* binary operators *)
   | Conditional of expr * expr * expr    (* if then else *)
@@ -62,12 +64,28 @@ let new_varname () : varid =
 let rec subst (var_name : varid) (repl : expr) (exp : expr) : expr =
   failwith "subst not implemented" ;;
 
-    
+let binop_to_string (b : binop) : string =
+  match b with 
+  | Plus     -> "Plus" 
+  | Minus    -> "Minus" 
+  | Times    -> "Times" 
+  | Equals   -> "Equals"
+  | LessThan -> "LessThan"
+
+
 (* exp_to_string -- Returns a string representation of the expr *)
 let rec exp_to_string (exp : expr) : string =
-  failwith "exp_to_string not implemented" ;;
+  failwith "exp_to_string not implemented" 
+  ;;
 
 (* exp_to_abstract_string: Returns a string representation of the abstract
    syntax of the expr *)
 let rec exp_to_abstract_string (exp : expr) : string =
-  failwith "exp_to_abstract_string not implemented" ;;
+  match exp with 
+  | Var v  -> sprintf "Var %s" v
+  | Num i  -> sprintf "Num %s" (string_of_int i)
+  | Bool b -> if b then "true" else "false"
+  | Unop (u, e) ->  sprintf "(Unop (Negate, %s))" (exp_to_string e)
+  | Binop (b, e1, e2) -> sprintf "(Binop (%s, %s, %s))" (binop_to_string b) (exp_to_string e1) (exp_to_string e2)
+  | Conditional (e1, e2, e3) -> sprintf "(Conditional (%s, %s, %s)" (exp_to_string e1) (exp_to_string e2) (exp_to_string e3)
+  ;;
