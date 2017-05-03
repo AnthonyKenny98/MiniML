2dfb25cebba74fdc72a968617c7180b4ecb76926
Anthony Kenny <Anthony@Anthonys-Macbook-Pro-2106.local>
2017-05-03 16:29:16 -0400

    all done

--- evaluation.ml
+++ evaluation.ml
@@ -52,14 +52,17 @@ module Env : Env_type =
     (* Looks up the value of a variable in the environment *)
     let rec lookup (env : env) (varname : varid) : value =
       match env with
-      | [] -> raise (EvalError ("Unbound Variable: " ^ varname))
-      | h :: t -> let var, valref = h in 
-        if var = varname then !valref else lookup t varname;;
+      | []     -> raise (EvalError ("Unbound Variable: " ^ varname))
+      | (var, valref) :: t -> if var = varname then !valref 
+                          else lookup t varname;;
 
     (* Returns a new environment just like env except that it maps the
        variable varid to loc *)
     let extend (env : env) (varname : varid) (loc : value ref) : env =
-      (varname, loc) :: env ;;
+      (varname, loc) :: env (* (List.filter (fun (x, _) -> not (x = varname)) env) *);;
+
+    let to_string (value_to_string : ?printenvp:bool -> value -> string) env  =
+      List.fold_left (fun _ (var, varref) -> sprintf "(%s : %s)" var (value_to_string !varref)) "" env;; 
 
     (* Returns a printable string representation of a value; the flag
        printenvp determines whether to include the environment in the
@@ -69,17 +72,16 @@ module Env : Env_type =
       | Val exp -> sprintf "Val (%s)" (exp_to_string exp)
       | Closure (exp, env) -> 
           let envp = if printenvp then 
-            List.fold_left (fun _ (var, varref) -> sprintf "(%s : %s)" var (value_to_string !varref)) "" env
-            else "env" in
+            to_string value_to_string env else "env" in
             sprintf "Closure (%s %s)" (exp_to_string exp) envp;;
 
    (* Returns a printable string representation of an environment *)
    let env_to_string (env : env) : string =
-      List.fold_left (fun _ (var, varref) -> sprintf "(%s : %s)" var (value_to_string !varref)) "" env;;
+      to_string value_to_string env;;
 
    let value_to_expr (value : value) : expr = 
     match value with 
-    | Val expr | Closure (expr, _)->  expr;;
+    | Val expr | Closure (expr, _) ->  expr;;
 
   end
 ;;
@@ -94,13 +96,23 @@ module Env : Env_type =
     the substitution model version or the dynamic or lexical
     environment model version. *)
 
-let exprs = Env.value_to_expr;;
+let vte = Env.value_to_expr;;
 
-let eval_x ?(subst = false) (exp : expr) (env : Env.env) (eval : expr -> Env.env -> Env.value)  = 
+let eval_x ?(subst = false) (exp : expr) (env : Env.env) 
+            (eval : expr -> Env.env -> Env.value) = 
   match exp with
-  | Unop (unop, e) -> Env.Val(Unop(unop, (exprs (eval e env))))
+  | Num _ | Unassigned 
+  | Raise | Bool _     -> Env.Val(exp)
+  | Unop (unop, e)     -> Env.Val(Unop(unop, (vte (eval e env))))
+  | App _              -> eval exp env
+  | Let (v, e1, e2)    -> eval e2 (Env.extend env v (ref (eval e1 env)))
+  | Letrec (v, e1, e2) -> eval e2 (Env.extend env v 
+                (ref (eval e1 (Env.extend env v (ref (Env.Val(Unassigned)))))))
+  | Var x -> 
+    (match Env.lookup env x with 
+    | Val y | Closure (y, _) -> Env.Val(y))  
   | Binop (binop, e1, e2) -> 
-    (match (exprs (eval e1 env)), (exprs(eval e2 env)) with
+    (match (vte (eval e1 env)), (vte (eval e2 env)) with
     | Num x, Num y -> 
       (match binop with
       | Plus     -> Env.Val(Num (x + y))
@@ -109,56 +121,55 @@ let eval_x ?(subst = false) (exp : expr) (env : Env.env) (eval : expr -> Env.env
       | Equals   -> Env.Val(Bool (x = y))
       | LessThan -> Env.Val(Bool (x < y)))
     | _,_ -> raise (EvalError ("Invalid Binop Expression: " ^ 
-            (exp_to_string (Binop(binop, exprs (eval e1 env), exprs (eval e2 env)))))))
+            (exp_to_string (Binop(binop, vte (eval e1 env), vte (eval e2 env)))))))
   | Conditional (condition, e1, e2) -> 
-      (match exprs (eval condition env) with 
+      (match vte (eval condition env) with 
       | Bool b -> if b then (eval e1 env) else (eval e2 env) 
       | _ -> raise (EvalError ("Invalid Condition: " ^ (exp_to_string condition)))) 
-  | _ -> if subst then Env.Val(exp) else (match exp with 
-
-  | Var x -> (match Env.lookup env x with 
-            | Val y | Closure (y, _) -> Env.Val(y))  
-  | Let (v, e1, e2) -> eval e2 (Env.extend env v (ref (eval e1 env)))
-  | Letrec (v, e1, e2) -> eval e2 (Env.extend env v (ref (eval e1 (Env.extend env v (ref (Env.Val(Unassigned)))))))
-  | App _ -> eval exp env
-  | Num _ | Unassigned | Raise | Bool _ -> Env.Val(exp)
-  | _ -> eval exp env)
+  | _ -> if subst then Env.Val(exp) else eval exp env
 
 let eval_t exp _env = exp ;;
 
 let rec eval_s exp env : Env.value = 
   let eval_s' e = eval_s e env in
   match exp with 
-  | Var x -> raise (EvalError ("Unbound Variable: " ^ x))
-  | Let (v, e1, e2) -> eval_s' (subst v e1 e2)
+  | Var x            -> raise (EvalError ("Unbound Variable: " ^ x))
+  | Let (v, e1, e2)  -> eval_s' (subst v e1 e2)
   | Letrec (x, v, p) -> eval_s' (subst x (subst x (Letrec(x, v, Var(x))) v) p)
   | App (f, e2) -> 
-    (match exprs (eval_s' f) with
+    (match vte (eval_s' f) with
     | Fun (x, p) -> eval_s' (subst x e2 p) 
-    | _ -> raise (EvalError (sprintf "This cannot be applied: %s is not a function" (exp_to_string f))))
+    | _      -> raise (EvalError (sprintf 
+            "This cannot be applied: %s is not a function" (exp_to_string f))))
   | _ -> eval_x exp env eval_s ~subst:true
 
 
 let rec eval_d exp env : Env.value =
 match exp with 
   | App (f, v) -> 
-    (match exprs (eval_d f env) with
+    (match vte (eval_d f env) with
     | Fun (x, e) -> eval_d e (Env.extend env x (ref (eval_d v env)))
-    | _ -> raise (EvalError (sprintf "This cannot be applied: %s is not a function" (exp_to_string f))))
-  | Fun (_) -> Val(exp)
-  | _ -> eval_x exp env eval_d
+    | _     -> raise (EvalError (sprintf 
+            "This cannot be applied: %s is not a function" (exp_to_string f))))
+  | Fun _ -> Val(exp)
+  | _     -> eval_x exp env eval_d
 ;;
 
 let rec eval_l exp env : Env.value = 
 match exp with 
-  | App (f, _) -> 
-    (match (eval_l f env) with
-    | Env.Closure (Fun (_, e), env1) -> eval_l e env1
-    | _ -> raise (EvalError (sprintf "This cannot be applied: %s is not a function" (exp_to_string f))))
+  | Var x -> 
+    (match Env.lookup env x with 
+    | Val y -> Env.Val(y)
+    | Closure (v, e) -> Env.Closure(v, e))
+  | App (f, v) -> 
+    (match (eval_l f env) with  
+    | Env.Closure (Fun (x, e), env1) -> eval_l e (Env.extend env1 x (ref (eval_d v env1)))
+    | _     -> raise (EvalError (sprintf 
+            "This cannot be applied: %s is not a function" (exp_to_string f))))
   | Fun (_) -> Env.close exp env 
-  | _ -> eval_x exp env eval_d
+  | _       -> eval_x exp env eval_l
 ;;
 
 
 
-let evaluate = eval_l ;;
+let evaluate = eval_d ;;
--- expr.ml
+++ expr.ml
@@ -49,35 +49,20 @@ let same_vars = SS.equal;;
 
 (* Generate a set of variable names from a list of strings (for
     testing purposes) *)
-let vars_of_list = SS.of_list ;;
-
-let binop_to_string (b : binop) : string =
-  match b with 
-  | Plus     -> "Plus" 
-  | Minus    -> "Minus" 
-  | Times    -> "Times" 
-  | Equals   -> "Equals"
-  | LessThan -> "LessThan"
-
-let binop_to_symbol (b : binop) : string =
-  match b with 
-  | Plus     -> "+" 
-  | Minus    -> "-" 
-  | Times    -> "*" 
-  | Equals   -> "="
-  | LessThan -> "<"
+let vars_of_list = SS.of_list;;
 
 (* Return a set of the variable names free in [exp] *)
 let rec free_vars (exp : expr)  : varidset =
  let filtervars exp v = SS.filter (fun x -> not (x = v)) (free_vars exp) in 
  match exp with 
-  | Var v  -> SS.singleton v
-  | Unop (_, e) -> free_vars e 
-  | Binop (_, e1, e2) | App (e1, e2) -> SS.union (free_vars  e1) (free_vars  e2)
-  | Conditional (e1, e2, e3) -> SS.union (free_vars  e1) (SS.union (free_vars e2) (free_vars  e3))
-  | Fun (v, e) ->  filtervars e v
-  | Let (v, e1, e2) -> SS.union (free_vars e1) (filtervars e2 v)
+  | Var v              -> SS.singleton v
+  | Unop (_, e)        -> free_vars e 
+  | Fun (v, e)         -> filtervars e v
+  | Let (v, e1, e2)    -> SS.union (free_vars e1) (filtervars e2 v)
   | Letrec (v, e1, e2) -> SS.union (filtervars e1 v) (filtervars e2 v)
+  | Binop (_, e1, e2) | App (e1, e2) -> SS.union (free_vars e1) (free_vars e2)
+  | Conditional (e1, e2, e3) -> 
+      SS.union (free_vars e1) (SS.union (free_vars e2) (free_vars e3))
   | _ -> SS.empty 
 ;;
   
@@ -88,77 +73,102 @@ let is_free_var var exp: bool =
 
 (* Return a fresh variable, constructed with a running counter a la
     gensym. Assumes no variable names use the prefix "var". *)
-let gensymcnt = ref 0;; 
+let gensymcnt = ref 0
+;; 
 
 let inc (i : int ref) : unit =
-  i := !i + 1;;
+  i := !i + 1
+;;
 
 let new_varname () : varid =
   let x = !gensymcnt in (inc gensymcnt);
-    string_of_int x ;;
-
+    string_of_int x
+;;
 
 (* Substitute [repl] for free occurrences of [var_name] in [exp] *)
 let rec subst (var_name : varid) (repl : expr) (exp : expr) : expr =
-  let aux = subst var_name repl in  
-    match exp with 
-    | Var v -> if v = var_name then repl else exp
-    | Unop (n, e) -> Unop (n, aux e)
-    | Binop (b, e1, e2) -> Binop (b, aux e1, aux e2)
-    | Conditional (e1, e2, e3) -> Conditional (aux e1, aux e2, aux e3)
-    | App (e1, e2) -> 
-        (match e1 with
-        | Fun (v, e) -> subst v (aux e2) (aux e)
-        | _ -> App (aux e1, aux e2))
-    | Fun (v, e) -> if v = var_name then exp
-      else if is_free_var v repl 
-        then let new_var = new_varname() in 
-          Fun (new_var, aux (subst v (Var (new_var)) e))
-        else Fun (v, aux e)
-    | Let (v, e1, e2) -> if v = var_name then Let (v, aux e1, e2)
-        else if is_free_var v repl 
-          then let new_var = new_varname() in
-            Let (new_var, aux e1, aux (subst v (Var new_var) e2))
-          else Let (v, aux e1, aux e2)
-    | Letrec (v, e1, e2) -> if v = var_name then Letrec (v, aux e1, e2)
-        else if is_free_var v repl 
-          then let new_var = new_varname() in
-            (Letrec (new_var, aux e1, aux (subst v (Var new_var) e2)))
-          else (Letrec (v, aux e1, aux e2))
-    | x -> x
+  let sub = subst var_name repl in  
+  match exp with 
+  | Var v                    -> if v = var_name then repl else exp
+  | Unop (n, e)              -> Unop (n, sub e)
+  | Binop (b, e1, e2)        -> Binop (b, sub e1, sub e2)
+  | Conditional (e1, e2, e3) -> Conditional (sub e1, sub e2, sub e3)
+  | App (e1, e2) -> 
+    (match e1 with
+      | Fun (v, e) -> subst v (sub e2) (sub e)
+      | _          -> App (sub e1, sub e2))
+  | Fun (v, e) -> 
+    if v = var_name then exp else 
+      if is_free_var v repl then let new_var = new_varname() in 
+        Fun (new_var, sub (subst v (Var (new_var)) e))
+      else Fun (v, sub e)
+  | Let (v, e1, e2) -> 
+    if v = var_name then Let (v, sub e1, e2) else 
+      if is_free_var v repl then let new_var = new_varname() in
+        Let (new_var, sub e1, sub (subst v (Var new_var) e2))
+      else Let (v, sub e1, sub e2)
+  | Letrec (v, e1, e2) -> 
+    if v = var_name then Letrec (v, sub e1, e2) else 
+    if is_free_var v repl then let new_var = new_varname() in
+      (Letrec (new_var, sub e1, sub (subst v (Var new_var) e2)))
+    else (Letrec (v, sub e1, sub e2))
+  | x -> x
 ;;
 
-(* exp_to_abstract_strin -- Returns a string representation of the expr *)
+
+(* Returns a string representation of the Binop operator *)
+let bin_str (b : binop) : string =
+  match b with 
+  | Plus     -> "Plus" 
+  | Minus    -> "Minus" 
+  | Times    -> "Times" 
+  | Equals   -> "Equals"
+  | LessThan -> "LessThan"
+
+(* Returns an algebraic symbol representation of the Binop operator as a string*)
+let binop_to_symbol (b : binop) : string =
+  match b with 
+  | Plus     -> "+" 
+  | Minus    -> "-" 
+  | Times    -> "*" 
+  | Equals   -> "="
+  | LessThan -> "<"
+
+(* exp_to_abstract_string -- Returns a string representation of the expr *)
 let rec exp_to_string (exp : expr) : string =
+  let s = exp_to_string in 
   match exp with
-  | Var v  -> v
-  | Num i  -> string_of_int i
-  | Bool b -> if b then "true" else "false"
-  | Unop (_, e) ->  sprintf "-%s" (exp_to_string e)
-  | Binop (b, e1, e2) -> sprintf "(%s %s %s)" (exp_to_string e1) (binop_to_symbol b) (exp_to_string e2)
-  | Conditional (e1, e2, e3) -> sprintf "if %s then %s else %s" (exp_to_string e1) (exp_to_string e2) (exp_to_string e3)
-  | Fun (v, e) -> sprintf "fun %s -> %s" v (exp_to_string e)
-  | Let (v, e1, e2) -> sprintf "let %s = %s in %s" v (exp_to_string e1) (exp_to_string e2)
-  | Letrec (v, e1, e2) -> sprintf "let rec %s = %s in %s" v (exp_to_string e1) (exp_to_string e2)
-  | Raise -> "Raise" (* gotta fix *)
-  | Unassigned -> "Unassigned" (* gotta fix *)
-  | App (e1, e2) -> sprintf "%s %s" (exp_to_string e1) (exp_to_string e2)
+  | Var v              -> v
+  | Num i              -> string_of_int i
+  | Bool b             -> if b then "true" else "false"
+  | Unop (_, e)        -> sprintf "-%s" (s e)
+  | Binop (b, e1, e2)  -> (s e1) ^ (binop_to_symbol b) ^ (s e2)
+  | Fun (v, e)         -> sprintf "fun %s -> %s" v (s e)
+  | Let (v, e1, e2)    -> sprintf "let %s = %s in %s" v (s e1) (s e2)
+  | Letrec (v, e1, e2) -> sprintf "let rec %s = %s in %s" v (s e1) (s e2)
+  | Raise              -> "Raise"
+  | Unassigned         -> "Unassigned"
+  | App (e1, e2)       -> (s e1) ^ (s e2)
+  | Conditional (e1, e2, e3) -> 
+                          sprintf "if %s then %s else %s" (s e1) (s e2) (s e3)
 ;;
 
 (* exp_to_abstract_string: Returns a string representation of the abstract
    syntax of the expr *)
 let rec exp_to_abstract_string (exp : expr) : string =
+  let s = exp_to_abstract_string in 
   match exp with 
-  | Var v  -> sprintf "Var(%s)" v
-  | Num i  -> sprintf "Num(%s)" (string_of_int i)
-  | Bool b -> if b then "Bool(true)" else "Bool(false)"
-  | Unop (_, e) ->  sprintf "Unop(Negate, %s)" (exp_to_abstract_string e)
-  | Binop (b, e1, e2) -> sprintf "Binop(%s, %s, %s)" (binop_to_string b) (exp_to_abstract_string e1) (exp_to_abstract_string e2)
-  | Conditional (e1, e2, e3) -> sprintf "Conditional(%s, %s, %s)" (exp_to_abstract_string e1) (exp_to_abstract_string e2) (exp_to_abstract_string e3)
-  | Fun (v, e) -> sprintf "Fun(%s, %s)" v (exp_to_abstract_string e)
-  | Let (v, e1, e2) -> sprintf "Let(%s, %s, %s)" v (exp_to_abstract_string e1) (exp_to_abstract_string e2)
-  | Letrec (v, e1, e2) -> sprintf "Letrec(%s, %s, %s)" v (exp_to_abstract_string e1) (exp_to_abstract_string e2)
-  | Raise -> "Raise" (* gotta fix *)
-  | Unassigned -> "Unassigned" (* gotta fix *)
-  | App (e1, e2) -> sprintf "App(%s, %s)" (exp_to_abstract_string e1) (exp_to_abstract_string e2)
+  | Var v              -> sprintf "Var(%s)" v
+  | Num i              -> sprintf "Num(%s)" (string_of_int i)
+  | Bool b             -> if b then "Bool(true)" else "Bool(false)"
+  | Unop (_, e)        -> sprintf "Unop(Negate, %s)" (s e)
+  | Binop (b, e1, e2)  -> sprintf "Binop(%s, %s, %s)" (bin_str b) (s e1) (s e2)
+  | Fun (v, e)         -> sprintf "Fun(%s, %s)" v (s e)
+  | Let (v, e1, e2)    -> sprintf "Let(%s, %s, %s)" v (s e1) (s e2)
+  | Letrec (v, e1, e2) -> sprintf "Letrec(%s, %s, %s)" v (s e1) (s e2)
+  | Raise              -> "Raise"
+  | Unassigned         -> "Unassigned" 
+  | App (e1, e2)       -> sprintf "App(%s, %s)" (s e1) (s e2)
+  | Conditional (e1, e2, e3) 
+                       -> sprintf "Conditional(%s, %s, %s)" (s e1) (s e2) (s e3)
   ;;
